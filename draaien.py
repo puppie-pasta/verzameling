215cd1343ae87898b79073ad37caa9cecfde0d2f
Florian Cornet <florian.cornet@hotmail.com>
2016-03-23 20:59:13 +0100

    dit is het siem

---
+++ draaien.py
@@ -0,0 +1,36 @@
+import pygame
+import pygame.locals
+ 
+white = (255, 255, 255)
+ 
+pygame.init()
+ 
+display = pygame.display.set_mode((300, 300))
+fpsClock = pygame.time.Clock()
+ 
+image = pygame.image.load('w.png')
+draai = 1400
+
+ecit = False
+ 
+while 1:
+
+ 
+    display.fill(white)
+
+    draai = 1
+    
+    image = pygame.transform.rotate(image, draai)
+    rect = image.get_rect()
+    rect.center = (150,150)
+ 
+    display.blit(image, rect)
+
+    for event in pygame.event.get():
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_q:
+          exit = True 
+
+
+    pygame.display.update()
+    fpsClock.tick(60)
